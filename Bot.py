dc6f171dcd3d263fc972791a651802a246a7c9b2
Bluntano <42609088+bluntano@users.noreply.github.com>
2020-01-05 01:10:45 +0200

    Update Bot.py
    - Every change: migrated to discord.py 1.x.x version from 0.16.12

--- Bot.py
+++ Bot.py
@@ -7,18 +7,18 @@ TOKEN = os.getenv("DISCORD")
 import discord
 import asyncio
 from discord.ext import commands
-from discord.ext.commands import Bot
-client = Bot(command_prefix = ["!"])
+client = commands.Bot(command_prefix = "!")
 
 import MIDIConverter as midic
 
 @client.event
 async def on_ready():
-    await client.change_presence(game=discord.Game(name='MIDI\'s'))
+    game = discord.Game("yes")
+    await client.change_presence(status=discord.Status.idle, activity=game)
     print("Weed is ready to serve!")
 
 @client.event
-async def on_command_error(error, ctx):
+async def on_command_error(ctx, error):
     if isinstance(error, commands.CommandOnCooldown):
         return
     elif isinstance(error, commands.CommandNotFound):
@@ -27,22 +27,23 @@ async def on_command_error(error, ctx):
         return
     raise error
 
-@client.command(pass_context=True)
-@commands.cooldown(1, 12, commands.BucketType.server)
-async def convert(ctx, link, sf=None, sr=22050):
-    channel = discord.utils.get(ctx.message.server.channels, name='midi-player', type=discord.ChannelType.text)
+@client.command()
+@commands.cooldown(1, 10, commands.BucketType.guild)
+async def convert(ctx, sf=None, sr=22050):
+    channel = discord.utils.get(ctx.message.guild.channels, name='midi-player', type=discord.ChannelType.text)
+    if ctx.message.channel != channel: return
     if ctx.message.author == client.user: return
     if ctx.message.author.bot == True: return
-    if ctx.message.channel != channel: return
-    if ctx.message.attachments == []: return
+    if ctx.message.attachments == []:
+        message = await ctx.send("❌ No MIDI file in the attachment!")
+        return
 
-    link = ctx.message.attachments[0]['url']
-    await client.send_typing(channel)
-    msg_status = await client.send_message(ctx.message.channel, "⏳ Checking the file...")
+    link = ctx.message.attachments[0].url
+    message = await ctx.send("⏳ Checking the file...")
 
     midic.detect_midi_file(link)
     if midic.detect_midi_file.is_midi == False:
-        await client.edit_message(msg_status, "❌ Not a valid MIDI file!")
+        await message.edit(content="❌ Not a valid MIDI file!")
         return
 
     soundfonts = ['megadrive', 'snes', 'n64']
@@ -55,7 +56,7 @@ async def convert(ctx, link, sf=None, sr=22050):
             sf = 'default'
     
     if not isinstance(sr, int):
-        await client.edit_message(msg_status, "❌ Not a valid sample rate!")
+        await message.edit(content="❌ Not a valid sample rate!")
         return
     
     if sr > 44100:
@@ -65,46 +66,40 @@ async def convert(ctx, link, sf=None, sr=22050):
 
     print('Converting with {} soundfont @ {} Hz...'.format(sf, sr))
 
-    msg_status = await client.edit_message(msg_status, "♻️ Uploading...")
+    await message.edit(content="♻️ Uploading...")
     midic.convert_midi_to_audio(link, sf, sr)
     while True:
         is_uploaded = midic.convert_midi_to_audio.is_downloaded
         if not is_uploaded:
             error = midic.convert_midi_to_audio.error
-            await client.edit_message(msg_status, "❗ Uploading failed:\n`{}`".format(error))
+            await message.edit(content="❗ Uploading failed:\n`{}`".format(error))
             break
         is_converted = midic.convert_midi_to_audio.is_converted
         if not is_converted:
             error = midic.convert_midi_to_audio.error
-            await client.edit_message(msg_status, "❗ Converting failed:\n`{}`".format(error))
+            await message.edit(content="❗ Converting failed:\n`{}`".format(error))
             break
         else:
-            await client.edit_message(msg_status, "✅ MIDI file converted!")
+            await message.edit(content="✅ MIDI file converted!")
             break
 
-@client.command(pass_context=True)
-@commands.cooldown(1, 12, commands.BucketType.server)
+@client.command()
+@commands.cooldown(1, 5, commands.BucketType.guild)
 async def play(ctx):
-    channel = discord.utils.get(ctx.message.server.channels, name='midi-player', type=discord.ChannelType.text)
-    if ctx.message.author == client.user: return
-    if ctx.message.author.bot == True: return
+    channel = discord.utils.get(ctx.message.guild.channels, name='midi-player', type=discord.ChannelType.text)
     if ctx.message.channel != channel: return
+    if ctx.message.author.bot: return
+    if ctx.message.author.id == client.user.id: return
 
-    user = ctx.message.author
-    voice_channel = user.voice.voice_channel
-    channel = None
-    if voice_channel != None:
-        channel=voice_channel.name
-        try:
-            vc = await client.join_voice_channel(voice_channel)
-            player = vc.create_ffmpeg_player('./weed.wav')
-            player.start()
-            while not player.is_done():
-                await asyncio.sleep(1)
-
-            player.stop()
-            await vc.disconnect()
-        except discord.ClientException as e:
-            print('error:', e)
+    if ctx.voice_client is None:
+        if ctx.author.voice:
+            await ctx.author.voice.channel.connect()
+            source = discord.PCMVolumeTransformer(discord.FFmpegPCMAudio('weed.wav'))
+            ctx.voice_client.play(source, after=lambda e: print('Player error: %s' % e) if e else None)
+        else:
+            await ctx.send("You are not connected to a voice channel.")
+            raise commands.CommandError("Author not connected to a voice channel.")
+    elif ctx.voice_client.is_playing():
+        ctx.voice_client.stop()
 
 client.run(TOKEN)
