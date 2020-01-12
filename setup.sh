610de836a9ebe576814caf2452bf08addfd7c259
Bluntano <42609088+bluntano@users.noreply.github.com>
2020-01-12 15:40:50 +0200

    Asks whether to make rc.local file or not
    A bit more dynamic now, and some code tweaks

--- setup.sh
+++ setup.sh
@@ -9,10 +9,34 @@ txtgrn=$(tput setaf 2) # Green
 txtred=$(tput setaf 1) # Red
 txtrst=$(tput sgr0)    # Resets color
 
+# If not root
+
 if [[ $EUID -ne 0 ]]; then
 	echo "${txtred}## This script must be run as root user! ##${txtrst}" 1>&2
 	exit 1
 fi
+
+# For making /etc/rc.local file
+
+rc_local () {
+	echo "${txtcyn}=== Making rc.local file... ===${txtrst}" # for starting up whenever machine boots up
+	echo "#!/bin/sh -e" > /etc/rc.local
+	echo "# rc.local" >> /etc/rc.local
+	echo "" >> /etc/rc.local
+	echo "exec 1>/tmp/rc.local.log 2>&1" >> /etc/rc.local
+	echo "set -x" >> /etc/rc.local
+	echo "cd $PWD && sh start.sh" >> /etc/rc.local
+	echo "" >> /etc/rc.local
+	echo "exit 0" >> /etc/rc.local
+	
+	chown root /etc/rc.local
+	chmod +x /etc/rc.local
+
+	echo "${txtcyn}=== Adding tail -f /tmp/rc.local.log to .bashrc file... ===${txtrst}"
+	echo "" >> ~/.bashrc
+	echo "# Checking rc.local logs when logging onto machine" >> ~/.bashrc
+	echo "tail -f /tmp/rc.local.log" >> ~/.bashrc
+}
 
 echo "${txtcyn}Installing required packages...${txtrst}"
 
@@ -108,27 +132,19 @@ if [ $success -eq 4 ]; then
 	echo "${txtcyn}=== chmod +x start.sh ===${txtrst}"
 	chmod +x start.sh
 	
-	echo "${txtcyn}=== Making rc.local file... ===${txtrst}" # for starting up whenever machine boots up
-	echo "#!/bin/sh -e" > /etc/rc.local
-	echo "# rc.local" >> /etc/rc.local
-	echo "" >> /etc/rc.local
-	echo "exec 1>/tmp/rc.local.log 2>&1" >> /etc/rc.local
-	echo "set -x" >> /etc/rc.local
-	echo "cd $PWD && sh start.sh" >> /etc/rc.local
-	echo "" >> /etc/rc.local
-	echo "exit 0" >> /etc/rc.local
-	
-	chown root /etc/rc.local
-	chmod +x /etc/rc.local
+	while true; do
+		read -p "Do you wish to launch the bot whenever the machine starts up?" yn
+		case $yn in
+			[Yy]* ) rc_local; break;;
+			[Nn]* ) exit;;
+			* ) echo "Please answer yes or no.";;
+		esac
+	done
 
-	echo "${txtcyn}=== Adding tail -f /tmp/rc.local.log to .bashrc file... ===${txtrst}"
-	echo "" >> ~/.bashrc
-	echo "# Checking rc.local logs when logging onto machine" >> ~/.bashrc
-	echo "tail -f /tmp/rc.local.log" >> ~/.bashrc
+	exit 0
 
 	echo "${txtgrn}=== All done! Exiting the script... ===${txtrst}"
 elif [ $success -lt 4 ]; then
 	echo "${txtred}=== Some packages are not installed, please try again ===${txtrst}"
+	exit 1
 fi
-
-exit 0
