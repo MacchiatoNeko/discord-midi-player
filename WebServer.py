ff71bcab1cbd6a2a9eff14194b75673edab9e6fd
Bluntano <42609088+bluntano@users.noreply.github.com>
2020-04-25 13:53:27 +0300

    Flask host to bind from .env file, add 404 error exception

--- WebServer.py
+++ WebServer.py
@@ -1,9 +1,17 @@
 # Web server stuff here
 from flask import Flask, redirect # redirect for Discord bot invite link (non-permanent solution for now)
 from threading import Thread
+import os
+from dotenv import load_dotenv # for .env file
 app = Flask(__name__)
+load_dotenv(verbose=True)
 
 client_userid = 0
+flask_host = os.getenv("FLASK_HOST")
+
+@app.errorhandler(404)
+def not_found(e):
+    return "4 + 0 = 4", 404
 
 @app.route("/")
 def hello():
@@ -11,7 +19,7 @@ def hello():
 
 # Start up web server
 def run_webserver():
-    app.run(host='0.0.0.0', port=80)
+    app.run(host=flask_host, port=80)
 
 # Creates a thread
 def keep_alive():
