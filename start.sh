f164d0892730639d7b0097ed52ea700a51436414
ellartdev <42609088+bluntano@users.noreply.github.com>
2021-07-20 02:13:18 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,14 +3,16 @@ currentVer=$(python3 --version)
 requiredVer="3.5.3"
 
 if [ "$(printf '%s\n' "$requiredVer" "$currentVer" | sort -V | head -n1)" = "$requiredVer" ]; then
-    python3 -m pip install --upgrade pip 2>&1 >/dev/null
+    export PYTHONUNBUFFERED=true
+    
+    python3 -m pip install --upgrade pip 2>&1
 
     # Remove debug.json file
     rm debug.json
 
     # Install the requirements
     # Uncomment the command below to install from the requirements file
-    python3 -m pip install -r requirements.txt 2>&1 >/dev/null
+    python3 -m pip install -r requirements.txt 2>&1
 
     # Finally, start the bot
     python3 Bot.py
